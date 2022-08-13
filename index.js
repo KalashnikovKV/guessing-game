54db0b3bedd372b916bbf115f52322c3242fe22b
KalashnikovKV <kirillrik1994@gmail.com>
2022-08-13 19:06:43 +0300

    feat: add g-game task

--- index.js
+++ index.js
@@ -1,3 +1,80 @@
 const GuessingGame = require('./src/guessing-game.js');
 
 window.game = new GuessingGame();
+/*let game = new GuessingGame();
+game.setRange(0, 4048)
+console.log(game.guess())
+game.lower()
+console.log(game.guess())
+game.lower()
+console.log(game.guess())
+game.lower()
+console.log(game.guess())
+game.greater()
+console.log(game.guess())
+game.greater()
+
+console.log(game.guess())
+game.greater()
+
+console.log(game.guess())
+game.lower()*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const number = 409;
+const game = new GuessingGame();
+game.setRange(0, 4048)
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+console.log(result)*/
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    min;
+    max;
+    middle;
+    constructor() {
+
+    }
 
     setRange(min, max) {
+        this.min = min
+        this.max = max
 
     }
 
     guess() {
+        this.middle = Math.ceil((this.min + this.max)/2)
+        return this.middle
 
     }
 
     lower() {
+        this.max = this.middle
 
     }
 
     greater() {
+        this.min = this.middle
 
     }
 }
